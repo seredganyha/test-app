f92b3fa83ba285775bf32aa8220d4ecd90d9378d
seredganyha <karix05@yandex.ru>
2023-04-28 23:53:07 +0300

    update: app template and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CardPizzaComponent } from './components/card-pizza/card-pizza.component';
+import { FormOrderComponent } from './components/form-order/form-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavHeaderComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardPizzaComponent,
+    FormOrderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
